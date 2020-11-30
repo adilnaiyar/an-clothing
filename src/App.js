8ec620a5bf6f7f42e3dbedccfcdd10fa43e1dd4f
Adil Naiyar <adil.naiyar@gail.com>
2020-11-30 20:14:00 +0530

    updating with shop page with collection preview and collItem component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import './App.css';
-
+import {Route, Switch} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shoppage.components';
+import './App.css';
 
 function App(){
   return (
       <div>
-        <HomePage />
+        <Switch>
+          <Route exact path = '/' component = {HomePage} />
+          <Route exact path = '/shop' component = {ShopPage} />
+        </Switch>
       </div>
-  )
+  );
 }
 
 export default App;
