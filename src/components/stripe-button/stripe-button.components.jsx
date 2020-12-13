1c422bda63796f6b4b9d13ff7c2b5b20aceedf8b
Adil Naiyar <adil.naiyar@gail.com>
2020-12-13 19:43:55 +0530

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.components.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HxaM7G63HECrUAUIESYv9PuGzwqDR4QzXfYelGoj1fbDY45olSd8rArbu1WTkIpBGVrKPE6rkCnhh2G1O8ZWj6k00qEyZYSp5';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='ANN Clothing '
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
