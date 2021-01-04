a2af862fe1ba687d8afdcc90321eccec863eeee2
Adil Naiyar <adil.naiyar@gail.com>
2021-01-04 14:04:39 +0530

    move the client side in client folder and added server side to manage the backend with server.js file and make an stripe backend to make charge from front end with stripe button

--- client/src/components/stripe-button/stripe-button.components.jsx
+++ client/src/components/stripe-button/stripe-button.components.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_51HxaM7G63HECrUAUIESYv9PuGzwqDR4QzXfYelGoj1fbDY45olSd8rArbu1WTkIpBGVrKPE6rkCnhh2G1O8ZWj6k00qEyZYSp5';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Succesful!');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
   };
 
   return (
@@ -17,7 +33,7 @@ const StripeCheckoutButton = ({ price }) => {
       billingAddress
       shippingAddress
       image='https://svgshare.com/i/CUz.svg'
-      description={`Your total is $${price}`}
+      description={`Your total is &#8377;{price}`}
       amount={priceForStripe}
       panelLabel='Pay Now'
       token={onToken}
